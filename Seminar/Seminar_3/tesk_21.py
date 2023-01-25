11acd9d56f2eace13d0b9d3d36927a389cdab45c
Andrey Petrov <kindlebooks2022@mail.ru>
2023-01-25 19:10:19 +0400

    homework_6

---
+++ Seminar/Seminar_3/tesk_21.py
@@ -0,0 +1,17 @@
+# Задача 21 
+# Напишите программу для печати всех уникальных значений в словаре.
+# input:
+#     [{'V':'S001'},{'V':'S002'},{'VI':'S001'},{'VI':'S005'},{'VII':'S005'},{'V':'S009'},{'VIII':'S007'}]
+    
+# output:
+#     {'S005','S002','S007','S007'}
+
+
+
+dictionary=({'V':'S001'},{'V':'S002'},{'VI':'S001'},{'VI':'S005'},{'VII':'S005'},{'V':'S009'},{'VIII':'S007'})
+# for item in dictionary:
+print(dictionary)
+    
+    
+# for i in dictionary:
+#     print(dictionary.items())
